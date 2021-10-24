a855dfe9b4ad0c6e4df63ad9475971bba6829656
Ivan19Aldana <ivanuchi189@gmail.com>
2021-10-23 22:23:09 -0600

    Crud Terminado con Roles Incluido

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//use \App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Listado de Usuarios
+//route::get('/','UserController@list');
+route::get("/",[\App\Http\Controllers\UserController::class,'list']);
+
+//Formulario Usuarios
+route::get("/form",[\App\Http\Controllers\UserController::class,'userform'])->name("usuarios.userform");
+
+//Guardar Usuarios
+route::post("/save",[\App\Http\Controllers\UserController::class,'save']);
+
+//Eliminar usuario
+route::delete("/delete/{id}",[\App\Http\Controllers\UserController::class,'delete'])->name('delete');
+
+//Formulario para editar usuarios
+route::get("/editform/{id}",[\App\Http\Controllers\UserController::class,'editform'])->name('editform');
+
+//Edicion de usuarios
+route::patch("/edit/{id}",[\App\Http\Controllers\UserController::class,'edit'])->name("edit");
